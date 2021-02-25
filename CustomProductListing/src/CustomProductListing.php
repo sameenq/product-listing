a76a0f8dd9e544dc1a9a04fc12a18b1bf0e46808
Sameen Qayyum <sq@elio-systems.com>
2021-02-25 18:35:49 +0100

    Initial Commit

---
+++ CustomProductListing/src/CustomProductListing.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace CustomProductListing;
+
+use Shopware\Core\Framework\Plugin;
+
+/**
+ * Class CustomProductListing
+ * @package CustomProductListing
+ * @author    Sameen Qayyum <qayyumsameen@gmail.com>
+ * @copyright Copyright (c) 2021
+ */
+class CustomProductListing extends Plugin
+{
+
+}
